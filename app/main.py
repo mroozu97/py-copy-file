02780e2bfaed0fced85bf483730debc460b405d0
Mateusz Mroz <mrozu10402@gmail.com>
2025-04-02 12:05:26 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 def copy_file(command: str) -> None:
-    if command.count(".txt") < 2:
-        return
-    if command.split(" ")[0] != "cp":
+    parts = command.split(" ")
+
+    if (len(parts) != 3 or parts[0] != "cp"
+            or not parts[1].endswith(".txt") or not parts[2].endswith(".txt")):
         return
 
-    file1 = command.split(" ")[1]
-    file2 = command.split(" ")[2]
+    file1 = parts[1]
+    file2 = parts[2]
 
     if file1 == file2:
         return
 
     try:
         with open(file1, "r") as file_in, open(file2, "w") as file_out:
-            read_content = file_in.read()
-            file_out.write(read_content)
+            # read_content = file_in.read()
+            file_out.write(file_in.read())
     except FileNotFoundError:
         print(f"Error: {file1} not found.")
     except Exception as e:
